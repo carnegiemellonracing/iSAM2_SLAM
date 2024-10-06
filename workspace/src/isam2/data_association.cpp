9c0e20a326dcb57c553f029b168121fa26efcf1e
dnguyen-23 <dnguyen2@andrew.cmu.edu>
2024-10-05 22:19:37 -0400

    bug with emplace_back; surrounding with print statements solves issue; otherwise code terminates

--- workspace/src/isam2/data_association.cpp
+++ workspace/src/isam2/data_association.cpp
@@ -29,8 +29,6 @@ void populate_m_dist(MatrixXd &global_cone_x, MatrixXd &global_cone_y,
             diff << global_cone_x(i,0) - slam_est.at(j).x(),
                     global_cone_y(i,0) - slam_est.at(j).y();
 
-            RCLCPP_INFO(logger, "mcov dims; rows: %d | cols: %d",
-                                slam_mcov.at(j).rows(), slam_mcov.at(j).cols());
             m_dist.push_back((diff * slam_mcov.at(j) * diff.transpose())(0, 0));
 
         }
@@ -49,24 +47,21 @@ void find_new_cones(vector<tuple<Point2, double, int>> &old_cones,
     int lo, hi = 0;
     for (int i = 0; i < cone_obs.size(); i++) {
         hi += n_landmarks + 1;
-        RCLCPP_INFO(logger, "Calculating min m_dist element");
         vector<double>::iterator min_dist = min_element(m_dist.begin() + lo,
                                                         m_dist.begin() + hi);
         int min_id = distance(m_dist.begin() + lo, min_dist);
 
         if (min_id == n_landmarks) {
-            RCLCPP_INFO(logger, "Adding new cone...");
-
+            //RCLCPP_INFO(logger, "adding new cone...");
             new_cones.emplace_back(cone_obs.at(i),
                                     bearing(i, 0),
                                     Point2(global_cone_x(i,0), global_cone_y(i,0)));
-            RCLCPP_INFO(logger, "New cone added!");
+            //RCLCPP_INFO(logger, "new cone added");
         } else {
-            RCLCPP_INFO(logger, "Updating old cone...");
+            //RCLCPP_INFO(logger, "updating old cone...");
             old_cones.emplace_back(cone_obs.at(i), bearing(i, 0), min_id);
-            RCLCPP_INFO(logger, "Old cone updated!");
+            //RCLCPP_INFO(logger, "old cone updated");
         }
-
 
         lo = hi;
     }
@@ -90,22 +85,20 @@ void data_association(vector<tuple<Point2, double, int>> &old_cones,
     calc_cone_range_from_car(range, cone_obs);
     calc_cone_bearing_from_car(bearing, cone_obs);
 
-    RCLCPP_INFO(logger, "Calculated cone range and bearing wrt car");
+    RCLCPP_INFO(logger, "calc bearing and range");
     cone_to_global_frame(range, bearing,
                          global_cone_x, global_cone_y,
                          cone_obs, cur_pose, prev_pose);
 
-    RCLCPP_INFO(logger, "Calculated global cone position");
-
+    RCLCPP_INFO(logger, "calc global cones");
     populate_m_dist(global_cone_x, global_cone_y, cone_obs.size(), m_dist,
                     slam_est, slam_mcov, logger);
-    RCLCPP_INFO(logger, "Populated m_dist");
 
+    RCLCPP_INFO(logger, "populated m_dist");
     find_new_cones(old_cones, new_cones,
                     global_cone_x, global_cone_y,bearing,
                     cone_obs, m_dist, n_landmarks, logger);
-    RCLCPP_INFO(logger, "Min ID performed, new cones identified");
-
+    RCLCPP_INFO(logger, "identified new cones");
 }
 
 
