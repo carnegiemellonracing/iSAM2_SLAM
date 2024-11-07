515ccf43a5f1a34b17bfe339079018fd3bb7293b
cmrdv <dale>
2024-11-07 13:30:19 -0500

    more tuning isam2 M_DIST_TH

--- workspace/src/isam2/data_association.cpp
+++ workspace/src/isam2/data_association.cpp
@@ -15,7 +15,7 @@
 
 #include "ros_utils.cpp"
 // const double M_DIST_TH = 1200; // Best for velocity motion_model
-const double M_DIST_TH = 50;
+const double M_DIST_TH = 40;
 
 using namespace Eigen;
 
