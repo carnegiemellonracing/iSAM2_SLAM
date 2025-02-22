57b189d683ada035317685f6a312c0f0db8782e9
thetechdude124 <aditya.dewan124@gmail.com>
2025-02-22 09:17:18 -0500

    INITIAL_SLAM_LOOKAHEAD

--- workspace/src/isam2/src/data_association.cpp
+++ workspace/src/isam2/src/data_association.cpp
@@ -3,6 +3,10 @@
  * @brief Perform data association in preparation for feeding data to SLAM
  */
 #include "data_association.hpp"
+#include "ros_utils.hpp"
+#include <cmath>
+#define _USE_MATH_DEFINES
+
 
 void populate_m_dist(MatrixXd &global_cone_x, MatrixXd &global_cone_y,
                     int num_obs, vector<double> &m_dist, double threshold,
@@ -92,15 +96,101 @@ void data_association(vector<tuple<Point2, double, int>> &old_cones,
                          global_cone_x, global_cone_y,
                          cone_obs, cur_pose);
     
-    
     populate_m_dist(global_cone_x, global_cone_y, cone_obs.size(), m_dist,
                     m_dist_th, slam_est, slam_mcov, logger);
 
     get_old_new_cones(old_cones, new_cones,
                     global_cone_x, global_cone_y,bearing,
                     cone_obs, m_dist, n_landmarks, logger);
-}
 
+    //Iterate through all cones; store the ones closest to being immediately to 
+    //the left and the right.
+    int leftConeIdx = 0
+    double leftConeRange = std::numeric_limits<double>::max()
+    int rightConeIdx = 0
+    double rightConeRange = std::numeric_limits<double>::max()
 
+    for (int i = 0; i < old_cones.size(); i++) {
 
+        //Check left
+        if (M_PI/2 - bearings(i, 0) < M_PI/2 - bearings(leftConeIdx, 0)
+            && range(i, 0) <= range(leftConeIdx, 0)) {
+            leftConeIdx = i;
+            leftConeRange = range(leftConeIdx, 0);
+        }
 
+        //Check right
+        if (-M_PI/2 - bearings(i, 0) < -M_PI/2 - bearings(rightConeIdx, 0)
+            && range(i, 0) <= range(rightConeIdx, 0)) {
+            rightConeIdx = i;
+            rightConeRange = range(rightConeIdx, 0);
+        }
+    }
+
+    //Now that we have the cones immediately to our left and right, find the cones
+    //with the bearings closest to zero
+    // sort left cones
+    std::vector<int> sortedLeftCones;
+    currConeIdx = leftConeIdx
+    size_t numSorted = 0
+    size_t totalCones = new_cones.size();
+    while (numSorted < (totalCones / 2)) {
+
+        Point2 leftConePos;
+        leftConePos.x = cur_pose.x + (range(leftConeIdx, 0) * std::cos(cur_pos.theta + bearings(leftConeIdx, 0)));
+        leftConePos.y = cur_pose.y + (range(leftConeIdx, 0) * std::sin(cur_pos.theta + bearings(leftConeIdx, 0)));
+
+        size_t leftCloseConeIdx = 0;
+        double leftMinDistance = INT_MAX();
+        size_t curr_idx = 0;
+
+        for (auto& input_cone : new_cones) {
+
+            double dx = input_cone.x - leftConePos.x;
+            double dy = input_cone.y - leftConePos.y;
+            double distance = std::sqrt(dx * dx + dy * dy);
+            if (distance < leftMinDistance) {
+                leftMinDistance = distance;
+                leftCloseConeIdx = curr_idx;
+            }
+            curr_idx = curr_idx + 1;
+
+        }
+        currConeIdx = leftCloseConeIdx;
+        sortedLeftCones.push_back(leftCloseConeIdx);
+        numSorted = numSorted + 1;
+    }
+
+    // sort right cones
+    std::vector<int> sortedRightCones;
+    currConeIdx = rightConeIdx
+    size_t numSorted = 0
+    while (numSorted < (totalCones / 2)) {
+        
+        Point2 rightConePos;
+        rightConePos.x = cur_pose.x + (range(rightConeIdx, 0) * std::cos(cur_pos.theta + bearings(rightConeIdx, 0)));
+        rightConePos.y = cur_pose.y + (range(rightConeIdx, 0) * std::sin(cur_pos.theta + bearings(rightConeIdx, 0)));
+
+        size_t rightCloseConeIdx = 0;
+        double rightMinDistance = INT_MAX();
+        size_t curr_idx = 0;
+
+        for (auto& input_cone : new_cones) {
+
+            double dx = input_cone.x - leftConePos.x;
+            double dy = input_cone.y - leftConePos.y;
+            double distance = std::sqrt(dx * dx + dy * dy);
+            if (distance < rightMinDistance) {
+                rightMinDistance = distance;
+                rightCloseConeIdx = curr_idx;
+            }
+            curr_idx = curr_idx + 1;
+            
+        }
+        currConeIdx = rightCloseConeIdx;
+        sortedRightCones.push_back(rightCloseConeIdx);
+        numSorted = numSorted + 1;
+    }
+};
+    //Starting with the left and right cones, project a series of vectors in front
+    //
