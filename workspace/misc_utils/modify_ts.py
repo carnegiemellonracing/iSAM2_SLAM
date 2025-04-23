f30e0421504e2b8e01af0ad0329f12386e2b7263
Daniel Nguyen <dnguyen2@andrew.cmu.edu>
2025-04-22 23:26:58 -0400

    updated modify_ts.py to trim

--- workspace/misc_utils/modify_ts.py
+++ workspace/misc_utils/modify_ts.py
@@ -1,3 +1,15 @@
+"""
+How to use:
+1.) Make sure the input bag and the
+output bag have been specified
+
+2.) Calculate the delta between the
+lidar timestamps and the odometry
+timestamps
+- This can be done by ros2 topic echo
+  and comparing the timesteps
+"""
+
 from pathlib import Path
 from rosbags.rosbag2 import Writer, Reader
 from rosbags.typesys import Stores, get_typestore, get_types_from_msg
@@ -6,13 +18,29 @@ import os
 import shutil
 from typing import TYPE_CHECKING, cast
 
-input_bag = '/home/dale/rosbags/fourth-run-mock-demo/'
-output_bag = '/home/dale/rosbags/fourth-run-mock-demo-stamped/'
+input_bag = 'second-run/'
+output_bag = 'minion-cranberry-juice/'
 
 # How were these calculated: ros2 topic echo /lidar_points and
 # /filter/velocity and then end the rosbag and analyze the timestamps
-delta_sec = 120587229
-delta_nanosec = 285524633
+# This is for fourth-run-mock-demo
+# delta_sec = 120587229
+# delta_nanosec = 285524633
+#
+# This is for third-run-demo-practice-five-laps
+delta_sec = 120583537
+delta_nanosec = 611264776
+
+# Used for trimming the rosbag
+# Times for fourth run stamped
+# start_trim = 1714592825
+# Personal notes 1714592862 is the start of the second lap
+# end_trim = 1714592899
+#
+# Times for third run stamped
+# start_trim = 1714503088 # The car hasn't started moving yet
+start_trim = 1714587692
+end_trim = 1714587992
 
 typestore = get_typestore(Stores.LATEST)
 custom_msg_paths = [
@@ -53,18 +81,24 @@ def modify_timestamps():
         for connection, timestamp, rawdata in reader.messages():
             msg = typestore.deserialize_cdr(rawdata, connection.msgtype)
 
+            # If the topic is /lidar_points, add the delta
             if connection.topic == '/lidar_points':
                 orig_sec = msg.header.stamp.sec
                 orig_nanosec = msg.header.stamp.nanosec
                 # print(orig_sec, orig_nanosec)
                 new_sec = orig_sec + delta_sec
                 new_nanosec = orig_nanosec + delta_nanosec
-                msg.header.stamp.sec = new_sec
-                msg.header.stamp.nanosec = new_nanosec
-                writer.write(connection_map[connection.id], timestamp,
+
+                if new_sec >= start_trim and new_sec <= end_trim:
+                    msg.header.stamp.sec = new_sec
+                    msg.header.stamp.nanosec = new_nanosec
+                    writer.write(connection_map[connection.id], timestamp,
                             typestore.serialize_cdr(msg, connection.msgtype))
             else:
-                writer.write(connection_map[connection.id], timestamp,
+                orig_sec = timestamp / 1e9
+                print("Timestamp seconds of non /lidar_points message", orig_sec)
+                if orig_sec >= start_trim and orig_sec <= end_trim:
+                    writer.write(connection_map[connection.id], timestamp,
                             typestore.serialize_cdr(msg, connection.msgtype))
         print("Finished creating rosbag")
 
